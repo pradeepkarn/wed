34b54d3d8983a4a4977c376d56f0b016a0edfb39
pradeepkarn <mail2pkarn@gmail.com>
2023-09-14 21:40:53 +0530

    slider translated

--- apps/view/components/common/sliders/slider-home-users.php
+++ apps/view/components/common/sliders/slider-home-users.php
@@ -9,10 +9,9 @@
                     $slider->tableName = "pk_user";
                     $sldrqry['user_group'] = "user";
                     $sldrqry['is_active'] = 1;
-                    if (USER && USER['gender']=='f') {
+                    if (USER && USER['gender'] == 'f') {
                         $sldrqry['gender'] = 'm';
-                    }
-                    else if (USER && USER['gender']=='m') {
+                    } else if (USER && USER['gender'] == 'm') {
                         $sldrqry['gender'] = 'f';
                     }
                     $slides = $slider->filter(assoc_arr: $sldrqry, limit: 20, ord: "RAND()");
@@ -51,69 +50,71 @@
                                                                                 echo "active";
                                                                             } ?>" style="height: 100%; background-image: url(/<?php echo MEDIA_URL; ?>/images/profiles/<?php echo $prof->cover; ?>); background-repeat: no-repeat; background-position: center; background-size: cover;">
                             <div style="position: relatve; height: 100%; background-color: rgba(0,0,0,0.3); ">
-                                
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-md-12">
 
-                                                <div class="card shadow" style="margin: 5px 70px 5px 70px;">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-12">
+
+                                            <div class="card shadow" style="margin: 5px 70px 5px 70px;">
 
 
-                                                    <div class="card-body">
-                                                        <div class="row">
-                                                            <div class="col-4">
-                                                                <a class="text-decoration-none profile-link" href="/<?php echo home . route('showPublicProfile', ['profile_id' => $prof->id]); ?>">
-                                                                    <img src="/<?php echo MEDIA_URL; ?>/images/profiles/<?php echo $prof->image; ?>" class="card-img-top profile-card-img" alt="<?php echo $prof->first_name; ?> <?php echo $prof->last_name; ?>">
-                                                                </a>
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="col-4">
+                                                            <a class="text-decoration-none profile-link" href="/<?php echo home . route('showPublicProfile', ['profile_id' => $prof->id]); ?>">
+                                                                <img src="/<?php echo MEDIA_URL; ?>/images/profiles/<?php echo $prof->image; ?>" class="card-img-top profile-card-img" alt="<?php echo $prof->first_name; ?> <?php echo $prof->last_name; ?>">
+                                                            </a>
+                                                        </div>
+                                                        <div class="col-8 text-start">
+                                                            <h5 class="card-title"><?php echo $prof->first_name; ?> <?php echo $prof->last_name; ?>
+                                                                <small>
+                                                                    (<?php echo bride_or_grom($prof->gender); ?>)
+                                                                </small>
+                                                            </h5>
+                                                            <?php
+                                                            $locked = lang('global')->locked ?? 'Locked';
+                                                            echo $prof->is_public == 1 ? null : "<span class='badge text-bg-warning'>{$locked}</span>"; ?>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->age ?? "Age"; ?>:</div>
+                                                                <div class="col">
+                                                                    <?php echo $prof->is_public == 1 ? $age : "**"; ?> <?php echo lang('global')->yrs ?? "Yrs"; ?>
+                                                                </div>
                                                             </div>
-                                                            <div class="col-8 text-start">
-                                                                <h5 class="card-title"><?php echo $prof->first_name; ?> <?php echo $prof->last_name; ?>
-                                                                    <small>
-                                                                        (<?php echo bride_or_grom($prof->gender); ?>)
-                                                                    </small>
-                                                                </h5>
-                                                                <?php echo $prof->is_public == 1 ? null : "<span class='badge text-bg-warning'>Private</span>"; ?>
-                                                                <div class="row">
-                                                                    <div class="col">Age:</div>
-                                                                    <div class="col">
-                                                                        <?php echo $prof->is_public == 1 ? getAgeFromDOB($prof->dob) : "**"; ?> Yrs
-                                                                    </div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Religion:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->religion : "****"; ?></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Caste:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->caste : "****"; ?></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Caste details:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->caste_detail : "****"; ?></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Location:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->address : "****"; ?></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Education:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->education : "****"; ?></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Profession:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->occupation : "****"; ?></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <div class="col">Annual Income:</div>
-                                                                    <div class="col"><?php echo $prof->is_public == 1 ? $prof->annual_income : "****"; ?> LPA</div>
-                                                                </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->religion ?? "Religion"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->religion : "****"; ?></div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->caste ?? "Caste"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->caste : "****"; ?></div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->caste_details ?? "Caste details"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->caste_detail : "****"; ?></div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->location ?? "Location"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->address : "****"; ?></div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->education ?? "Education"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->education : "****"; ?></div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->profession ?? "Profession"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->occupation : "****"; ?></div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col"><?php echo lang('global')->annual_income ?? "Annual income"; ?>:</div>
+                                                                <div class="col"><?php echo $prof->is_public == 1 ? $prof->annual_income : "****"; ?> <?php echo lang('global')->lpa ?? "LPA"; ?></div>
                                                             </div>
                                                         </div>
-
                                                     </div>
 
-
                                                 </div>
+
+
+                                            </div>
 
 
 
