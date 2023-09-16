b93ab2986b46b0c82f939ff2290ac502a8c05170
pradeepkarn <mail2pkarn@gmail.com>
2023-09-16 19:58:53 +0530

    main ctrl

--- controllers/Main_ctrl.ctrl.php
+++ controllers/Main_ctrl.ctrl.php
@@ -1,17 +1,22 @@
-<?php 
+<?php
 class Main_ctrl
 {
     public $post;
     public $get;
     public $files;
-    public function __construct() {
+    public function __construct()
+    {
         $this->post = obj($_POST);
         $this->get = obj($_GET);
-        $this->files = isset($_FILES)?obj($_FILES):null;
+        $this->files = isset($_FILES) ? obj($_FILES) : null;
     }
 
     public function render_main($context = null)
     {
         import("apps/view/layouts/main.php", $context);
+    }
+    public function render_layout($context = null, $layout = "apps/view/layouts/main.php")
+    {
+        import($layout, $context);
     }
 }
